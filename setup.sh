ea19a24d67e5d3cc96146fbc93e16c42bc873521
MasterofDeath01 <sri.koustubh@gmail.com>
2026-05-24 13:53:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -236,7 +236,7 @@ defaults_write_if_needed com.apple.dock autohide-delay -int 0
 defaults_write_if_needed com.apple.dock autohide-time-modifier -float 0.5
 
 # Remove all apps from dock
-defaults_write_if_needed com.apple.dock persistent-apps -array
+defaults write com.apple.dock persistent-apps '()'
 
 # Disable "Show suggested and recent apps in Dock"
 defaults_write_if_needed com.apple.dock show-recents -bool false
